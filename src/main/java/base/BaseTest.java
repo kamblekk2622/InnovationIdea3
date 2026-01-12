f3d31c34a7d9defd7502906b85f45b5d37598f26
Kiran kamble <kamblekk2622@gmail.com>
2026-01-12 10:17:03 +0530

    log 4j2 added to the project for finding application logs

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,20 +2,26 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utils.Log;
 
 public class BaseTest {
- protected WebDriver driver;
+    protected WebDriver driver;
  @BeforeMethod
  public void setUp(){
+     Log.info("Starting Webdriver...");
      driver=new ChromeDriver();
      driver.manage().window().maximize();
+     Log.info("Navigate to the login page");
      driver.get("https://admin-demo.nopcommerce.com/login");
  }
  @AfterMethod
  public void tearDown(){
      if(driver!=null){
+         Log.info("Closing browser...");
          driver.quit();
      }
  }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,18 +1,24 @@
 package tests;
 
 import base.BaseTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
+    private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
+
     @Test
     public void testValidLogin(){
+        Log.info("starting login test...");
         LoginPage loginpage=new LoginPage(driver);
         loginpage.enterUsername("admin@yourstore.com");
         loginpage.enterPassword("admin");
         loginpage.clickOnLoginButton();
-        System.out.println("Title of the page "+driver.getTitle());
+        Log.info("Title of the page "+driver.getTitle());
 //        Assert.assertEquals(driver.getTitle(),"Dashboard / nopCommerce administration");
     }
 }
