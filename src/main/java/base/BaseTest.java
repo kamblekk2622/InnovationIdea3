36c460612c8e55bbd934df4efaf5be2b9b3d82b4
Kiran kamble <kamblekk2622@gmail.com>
2026-01-12 13:11:21 +0530

    Report with screen shot added into project

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,29 +1,57 @@
 package base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.internal.TestResult;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class BaseTest {
     protected WebDriver driver;
- @BeforeMethod
- public void setUp(){
-     Log.info("Starting Webdriver...");
-     driver=new ChromeDriver();
-     driver.manage().window().maximize();
-     Log.info("Navigate to the login page");
-     driver.get("https://admin-demo.nopcommerce.com/login");
- }
- @AfterMethod
- public void tearDown(){
-     if(driver!=null){
-         Log.info("Closing browser...");
-         driver.quit();
-     }
- }
+    protected static ExtentReports extent;
+    protected ExtentTest test;
+
+    @BeforeSuite
+    public void setupReport() {
+        extent = ExtentReportManager.getReportInstance();
+    }
+
+    @AfterSuite
+    public void tearDownReport() {
+        extent.flush();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        Log.info("Starting Webdriver...");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Log.info("Navigate to the login page");
+        driver.get("https://admin-demo.nopcommerce.com/login");
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        if(result.getStatus() == ITestResult.FAILURE) {
+
+            String screenshotPath = ExtentReportManager.captureScreenshot(driver, "LoginFailure");
+            test.fail("Test Failed.. Check Screenshot",
+                    MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+        }
+        if (driver != null) {
+            Log.info("Closing browser...");
+            driver.quit();
+        }
+    }
 
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -6,19 +6,39 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest {
     private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
 
-    @Test
-    public void testValidLogin(){
+    @Test(priority = 1)
+    public void testValidLogin() {
         Log.info("starting login test...");
-        LoginPage loginpage=new LoginPage(driver);
+        test = ExtentReportManager.createTest("Login Test");
+        test.info("Nevigating to URL");
+        LoginPage loginpage = new LoginPage(driver);
+        test.info("Adding creadentails");
         loginpage.enterUsername("admin@yourstore.com");
         loginpage.enterPassword("admin");
+        test.info("Clicking login button");
         loginpage.clickOnLoginButton();
-        Log.info("Title of the page "+driver.getTitle());
-//        Assert.assertEquals(driver.getTitle(),"Dashboard / nopCommerce administration");
+        Log.info("Title of the page " + driver.getTitle());
+        test.pass("Login Successful");
+    }
+    @Test(priority = 2)
+    public void testLoginWithInValid() {
+        Log.info("starting login test...");
+        test = ExtentReportManager.createTest("Login Test With Invalid credential");
+        test.info("Nevigating to URL");
+        LoginPage loginpage = new LoginPage(driver);
+        test.info("Adding creadentails");
+        loginpage.enterUsername("admin@yourstore.com");
+        loginpage.enterPassword("admin43254324");
+        test.info("Clicking login button");
+        loginpage.clickOnLoginButton();
+        Log.info("Title of the page " + driver.getTitle());
+        Assert.assertEquals(driver.getTitle(),"hjdvsv");
+        test.pass("Login Successful");
     }
 }
